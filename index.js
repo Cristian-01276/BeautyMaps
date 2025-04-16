41ac410176a7cb6146f62640b138208382476d41
Santiago Cruz <71169279+cruz3103@users.noreply.github.com>
2025-04-16 11:39:09 -0500

    Feat/profesionals (#2)
    * Se hace home del proyecto
    
    * se modifica logo
    
    * Contenido añadido
    
    * feat: Se crean las carpetas catalogo, mi cuenta y citas pendintes(con sus respectivos html´s y sus estilos
    
    ---------
    
    Co-authored-by: LaRola222 <paola.andrea.222rodriguez@gmail.com>

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ abrir.addEventListener("click",() =>{
 cerrar.addEventListener("click",() =>{
     nav.classList.remove("visible");    
 })
+/*botones de aceptar y rechazar ciras en citas pendientes*/
+document.addEventListener("DOMContentLoaded", function () {
+    const botonesAceptar = document.querySelectorAll(".accept");
+    const botonesRechazar = document.querySelectorAll(".decline");
+
+    botonesAceptar.forEach((boton) => {
+      boton.addEventListener("click", function () {
+        alert("✅ ¡Cita agendada exitosamente!");
+      });
+    });
+
+    botonesRechazar.forEach((boton) => {
+      boton.addEventListener("click", function () {
+        alert("❌ Cita rechazada");
+      });
+    });
+  });
+
